53632793d7fc7ea6ee9a3d3f3772db09860d4f30
habibi99-coder <habiburgh3@gmail.com>
2026-06-10 16:54:08 -0400

    Create account table with constraints and defaults

--- sql/03_create_account_table.sql
+++ sql/03_create_account_table.sql
@@ -1,1 +1,27 @@
+USE SmartBankDB;
+GO
+
+CREATE TABLE account
+(	
+	account_number INT PRIMARY KEY,
+	type_of_account VARCHAR(20) NOT NULL,
+	balance DECIMAL (12,2) NOT NULL
+		CONSTRAINT df_account_balance DEFAULT 0.00,
+	date_opened DATE NOT NULL,
+	customer_id INT NOT NULL,
+
+	CONSTRAINT fk_account_customer 
+		FOREIGN KEY (customer_id)
+		REFERENCES customer(customer_id),
+
+	CONSTRAINT chk_type_of_account
+		CHECK (type_of_account IN ('CHECKING','SAVINGS','BOND')),
+
+	CONSTRAINT chk_account_balance
+		CHECK(balance >= 0)
+
+
+);
+GO
+
 
