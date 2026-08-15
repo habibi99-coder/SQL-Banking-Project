7b8342bf1f631b534b99c97fd19e332b6369f7ea
habibi99-coder <habiburgh3@gmail.com>
2026-08-14 22:57:43 -0400

    Reset test data and manage transaction triggers
    Disable and re-enable trigger around transaction reset operations.

--- sql/00_reset_test_data.sql
+++ sql/00_reset_test_data.sql
@@ -1,20 +1,22 @@
 USE SmartBankDB;
 GO
 
--- Clear transaction test records.
+DISABLE TRIGGER trg_PreventTransactionChanges ON account_transaction;
+GO
+
 DELETE FROM account_transaction;
 GO
 
--- Reset transaction identity back to 1.
 DBCC CHECKIDENT ('account_transaction', RESEED, 0);
 GO
 
--- Reset account balances for clean testing.
 UPDATE account
 SET balance = 0.00;
 GO
 
--- Confirm reset results.
+ENABLE TRIGGER trg_PreventTransactionChanges ON account_transaction;
+GO
+
 SELECT *
 FROM account_transaction;
 GO
